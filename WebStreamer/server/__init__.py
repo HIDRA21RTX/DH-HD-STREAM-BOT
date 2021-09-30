a0f8f706ea5e7582f43f16e7bf083ebe10edda63
HIDRA21RTX <89720681+HIDRA21RTX@users.noreply.github.com>
2021-09-29 21:38:18 -0400

    Create __init__.py

---
+++ WebStreamer/server/__init__.py
@@ -0,0 +1,16 @@
+# © GuChiDevStudio.Ltda [ @CLAY_MODS ] [ Telegram ]
+
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+
+from aiohttp import web
+
+from .stream_routes import routes
+
+async def web_server():
+
+    web_app = web.Application(client_max_size=30000000)
+
+    web_app.add_routes(routes)
+
+    return web_app
+
